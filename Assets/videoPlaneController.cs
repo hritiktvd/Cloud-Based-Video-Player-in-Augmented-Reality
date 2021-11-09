0dcfbc279cde3b3b6bc0d483490ea5ffa2610cb5
hriti <hritik2599@gmail.com>
2021-11-09 17:19:30 -0500

    GCERT Project First Commit

---
+++ Assets/videoPlaneController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class videoPlaneController : MonoBehaviour {
+
+    public VideoPlayer vp;
+    public Texture defaultTex;
+    public RenderTexture renderTex;
+
+    private void OnEnable()
+    {
+        if (vp != null) vp.Play();
+    }
+
+    private void OnDisable()
+    {
+        if (vp != null)
+        {
+            vp.Stop();
+            Graphics.Blit(defaultTex, renderTex);
+        }
+    }
+
+
+}
